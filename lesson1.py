81a6cb9502d746ed1c377207a6b6703f6e8f1a1f
maxim <maksdoneckiy320@gmail.com>
2023-06-23 18:18:55 +0300

    'palindrome'

--- lesson1.py
+++ lesson1.py
@@ -8,11 +8,26 @@ def strcounter(s):
         print(i, '-', c)
 strcounter('aasdfgddffgghhg')'''
 
-def superc(s):
+'''def superc(s):
     sc = {}
     for i in s:
         sc[i] = sc.get(i, 0) + 1
     for i, c in sc.items():
         print(i, '-', c)
 
-superc('addddsffggggfddssas')
+superc('addddsffggggfddssas' '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')'''
+
+
+
+def palind(s):
+    c = 0
+    for i in range(len(s)//2):
+        if s[i] == s[-(i+1)]:
+            c += 1
+    if c == len(s)//2:
+        return True
+    else:
+        return False
+
+
+print(palind('лепсспел'))
