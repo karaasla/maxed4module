d50d9ebcfb584ed99179342ddfbbcebf87eed88b
maxim <maksdoneckiy320@gmail.com>
2023-07-28 23:04:38 +0300

    lesson 7

--- dsite/app_dsite/models.py
+++ dsite/app_dsite/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Dsite(models.Model):
+    title = models.CharField("заголовок", max_length=128)
+    description = models.TextField("описание")
+    price = models.DecimalField("цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "advertisements"
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, title={self.title}, price={self.price})"
+
+
+
+
+
+
+
